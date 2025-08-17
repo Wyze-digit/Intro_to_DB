026612eb10035e7544db2da400b9afff131145b7
Wisdom Emmanuel <ifiokbassey.e@gmail.com>
2025-08-17 23:51:39 +0100

    space added in addresses

--- task_6.sql
+++ task_6.sql
@@ -11,8 +11,8 @@ USE alx_book_store;
 INSERT INTO customer (customer_id, customer_name, email, address)
 VALUES
     (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness  Ave.'),
-    (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
-    (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
+    (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness  Ave.'),
+    (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness  Ave.');
 
 -- After running this script, three new rows will be added to the 'customer' table
 --   Customer 2: Blessing Malik
