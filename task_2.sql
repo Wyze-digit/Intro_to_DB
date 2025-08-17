e38d75aa7a31c8cf20428b26cff5b401abb94fc0
Wisdom Emmanuel <ifiokbassey.e@gmail.com>
2025-08-17 23:39:15 +0200

    corrections done on order detail code

--- task_2.sql
+++ task_2.sql
@@ -38,13 +38,13 @@ CREATE TABLE orders (
 );
 
 -- Create Order Details table
-CREATE TABLE order_details (
+CREATE TABLE Order_Details (
     orderdetailid INT PRIMARY KEY,
     order_id INT,
     book_id INT,
     quantity DOUBLE,
-    FOREIGN KEY (order_id) REFERENCES orders(order_id),
-    FOREIGN KEY (book_id) REFERENCES books(book_id)
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
 
 
