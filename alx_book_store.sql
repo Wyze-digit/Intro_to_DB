72f0072e3d036ef0e05071532a74744359d8340f
Wisdom Emmanuel <ifiokbassey.e@gmail.com>
2025-08-17 22:06:42 +0200

    all codes in sql written

---
+++ alx_book_store.sql
@@ -0,0 +1,51 @@
+-- alx_book_store.sql
+
+-- 1. CREATE DATABASE
+CREATE DATABASE alx_book_store;
+
+-- 2. SELECT THE DATABASE
+USE alx_book_store;
+
+-- 3. CREATE TABLE Authors
+CREATE TABLE Authors (
+    author_id INT PRIMARY KEY AUTO_INCREMENT,
+    author_name VARCHAR(215) NOT NULL
+);
+
+-- 4. CREATE TABLE Books
+CREATE TABLE Books (
+    book_id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(130) NOT NULL,
+    author_id INT,
+    price DOUBLE,
+    publication_date DATE,
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id)
+);
+
+-- 5. CREATE TABLE Customers
+CREATE TABLE Customers (
+    customer_id INT PRIMARY KEY AUTO_INCREMENT,
+    customer_name VARCHAR(215) NOT NULL,
+    email VARCHAR(215) UNIQUE,
+    address TEXT
+);
+
+-- 6. CREATE TABLE Orders
+CREATE TABLE Orders (
+    order_id INT PRIMARY KEY AUTO_INCREMENT,
+    customer_id INT,
+    order_date DATE,
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+);
+
+-- 7. CREATE TABLE Order_Details
+CREATE TABLE Order_Details (
+    orderdetailid INT PRIMARY KEY AUTO_INCREMENT,
+    order_id INT,
+    book_id INT,
+    quantity DOUBLE,
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
+);
+
+
