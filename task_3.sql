d25812d10b170916e1abddefab360d9473ea3d6a
Wisdom Emmanuel <ifiokbassey.e@gmail.com>
2025-08-17 23:50:57 +0200

    corrected database name to be used in code

--- task_3.sql
+++ task_3.sql
@@ -1,4 +1,9 @@
 -- task_3.sql
 -- This script lists all tables in the specified database
 
+
+-- Switch to the correct database
+USE alx_book_store;
+
+-- List all tables in the database
 SHOW TABLES;
