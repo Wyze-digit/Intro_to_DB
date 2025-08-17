e14cba3b33e242d839adc76fd74ee56f089a3cf2
Wisdom Emmanuel <ifiokbassey.e@gmail.com>
2025-08-17 23:48:27 +0100

    customer id numbers included in code

--- task_6.sql
+++ task_6.sql
@@ -7,8 +7,9 @@ USE alx_book_store;
 -- We specify the columns where data will be stored:
 --   customer_id, customer_name, email, address
 -- Then we provide multiple sets of values (one set per customer)
+
 INSERT INTO customer (customer_id, customer_name, email, address)
-VALUES 
+VALUES
     (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
     (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
     (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
