8564fbefe46ef6895fe1e14058b39ad926de1258
Wisdom Emmanuel <ifiokbassey.e@gmail.com>
2025-08-17 23:35:21 +0200

    errors on orders table corrected

--- task_2.sql
+++ task_2.sql
@@ -34,7 +34,7 @@ CREATE TABLE orders (
     order_id INT PRIMARY KEY,
     customer_id INT,
     order_date DATE,
-    FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
 -- Create Order Details table
