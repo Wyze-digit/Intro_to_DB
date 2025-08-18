b1bbe11f590c5fd77af4726acdbfa3b6e5e6459e
Wisdom Emmanuel <ifiokbassey.e@gmail.com>
2025-08-18 10:37:24 +0100

    removed all the codes

--- task_4.sql
+++ task_4.sql
@@ -1,27 +1,3 @@
--- solve
 -- Select the database we want to work in
-USE alx_book_store;
-
--- Script to print the full description of the table 'books' from the database 'alx_book_store'
--- The database name will be passed as an argument of the mysql command
--- Usage: mysql -u username -p alx_book_store < task_4.sql
-
--- do not use DESCRIBE books;
-
--- Query the INFORMATION_SCHEMA.COLUMNS system table to get the full schema of the 'books' table
--- This provides details similar to DESCRIBE, without using the forbidden keywords
-SELECT 
-    COLUMN_NAME AS 'Column Name',       -- Name of the column (e.g., book_id, title)
-    COLUMN_TYPE AS 'Data Type',         -- Data type of the column (e.g., INT, VARCHAR(255))
-    IS_NULLABLE AS 'Nullable',          -- Indicates whether NULL values are allowed (YES/NO)
-    COLUMN_KEY AS 'Key',                -- Shows if it is a PRIMARY KEY, UNIQUE, etc.
-    COLUMN_DEFAULT AS 'Default',        -- Default value for the column (if any)
-    EXTRA AS 'Extra'                    -- Extra info like auto_increment
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = 'alx_book_store'  -- Filter for the database
-  AND TABLE_NAME = 'books';            -- Filter for the table 'books'
-
--- Running this query will output a list of columns in the 'books' table along with:
--- column name, data type, nullability, key info, default value, and extra details
 
 
