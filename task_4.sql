891f4fb39d75642bc7d6ab5196bcee262b1ebe45
Wisdom Emmanuel <ifiokbassey.e@gmail.com>
2025-08-18 10:38:29 +0100

    re-write codes for the task

--- task_4.sql
+++ task_4.sql
@@ -1,3 +1,10 @@
 -- Select the database we want to work in
 
+USE alx_book_store;
+
+SELECT TABLE_NAME, COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_DEFAULT, EXTRA
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'alx_book_store'
+  AND TABLE_NAME = 'Books';
+
 
